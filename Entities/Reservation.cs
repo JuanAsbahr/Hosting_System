af5d3c515d4dc1f69e577875192b6e3c8efa7988
JuanAsbahr <juan_asbahr@hotmail.com>
2023-07-03 15:28:43 -0300

    Test Reservation

--- Entities/Reservation.cs
+++ Entities/Reservation.cs
@@ -20,6 +20,12 @@
             return (int)duration.TotalDays;
         }
 
+        public void UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
         public override string ToString()
         {
             return "Room "
